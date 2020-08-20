d09f2bb4dcb70a123108c212826252f189106f44
zosolml <adityapaul76@gmail.com>
2020-08-20 17:34:19 +0530

    Add files via upload

--- todosite/settings.py
+++ todosite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import psycopg2
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,3 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+          os.path.join(BASE_DIR,'static')
+          ]
+
+STATIC_ROOT=os.path.join(BASE_DIR,'assests')
